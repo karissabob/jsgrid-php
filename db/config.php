f3d34a87ceef2466adb763500a1805318bddc8a4
Karissa <karissabob@hotmail.com>
2019-01-09 22:44:21 +0000

    Update config.php
    Updated database details to be specific to my set up. Proven to work locally.

--- db/config.php
+++ db/config.php
@@ -1,9 +1,9 @@
 <?php
 
 return array(
-    "db" => "mysql:host=127.0.0.1;dbname=jsgridsample",
+    "db" => "mysql:host=localhost;dbname=jsgridsample",
     "username" => "root",
-    "password" => "mysql"
+    "password" => ""
 );
 
 ?>
